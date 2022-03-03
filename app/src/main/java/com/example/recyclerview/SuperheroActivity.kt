701611ff88d145d30160c7ff696b2298d218df60
unknown <naufalyanuaradhi27@gmail.com>
2022-03-03 19:53:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/SuperheroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recyclerview
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SuperheroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout
+    }
+}
